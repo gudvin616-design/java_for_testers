30f0e317d7f29a569c6517947754d8102e97b5bc
gudvin <georgijchirkov6161126g@gmail.com>
2026-03-02 22:21:02 +0300

    Выражения и операции

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,9 @@
+public class Geometry {
+    public static void main(String[] args) {
+        var side = 7.0;
+        System.out.println("площадь квадрата со стороной " + side + "=" + (side*side));
+
+
+
+    }
+}
--- sandbox/src/main/java/hello.java
+++ sandbox/src/main/java/hello.java
@@ -1,5 +1,12 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2+2*2);
+        System.out.println("helo, " + "world!");
+        System.out.println("2+2 = " + (2+2));
     }
 }
